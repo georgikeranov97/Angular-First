cb9bd3dc221569f2e7e017f1041c7c7144bc3008
gosho <gosho970307@gmail.com>
2019-06-25 17:11:01 +0300

    added some reactive forms

--- my-first-project/src/app/posts/add-post/add-post.component.ts
+++ my-first-project/src/app/posts/add-post/add-post.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../../post';
+import { map } from 'rxjs/internal/operators';
+import { Router } from '@angular/router';
+// import { map } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'app-add-post',
@@ -7,12 +12,36 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./add-post.component.sass']
 })
 export class AddPostComponent implements OnInit {
-
-  name = new FormControl('');
-
-  constructor() { }
+  profileForm = new FormGroup({
+    title: new FormControl(''),
+    body: new FormControl(''),
+  })
+  item: Post;
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
+  }
+
+  addNewPost() {
+    this.httpClient.post('https://jsonplaceholder.typicode.com/posts/', {
+      title: 'My first post',
+      body: 'This is my very first post',
+    }).pipe(map((res: any) => {
+        return new Post(res);
+    })).subscribe((res: Post) => {
+      console.log(2, res);
+      this.item = res;
+      this.router.navigateByUrl('/posts');
+    });
+
+  }
+  
 
 }
--- my-first-project/src/app/posts/posts.module.ts
+++ my-first-project/src/app/posts/posts.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { IndexComponent } from './index/index.component';
 import { AddPostComponent } from './add-post/add-post.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
     declarations: [IndexComponent, AddPostComponent],
     imports: [
       CommonModule,
-      PostsRoutingModule
+      PostsRoutingModule,
+      ReactiveFormsModule,
     ]
   })
 
