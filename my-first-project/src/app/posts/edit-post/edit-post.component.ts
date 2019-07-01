cac61c8d9b3f525b28c5100f0aafa37b69b08402
gosho <gosho970307@gmail.com>
2019-07-01 17:49:38 +0300

    changes in edit

--- my-first-project/src/app/posts/edit-post/edit-post.component.ts
+++ my-first-project/src/app/posts/edit-post/edit-post.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/post';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/internal/operators';
 
@@ -12,19 +12,20 @@ import { map } from 'rxjs/internal/operators';
 })
 export class EditPostComponent implements OnInit {
   item: Post;
-  
-  profileForm = new FormGroup({
-    title: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    body: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
-  })
-
+  public profileForm;
   constructor(
     private httpClient: HttpClient,
     private router: Router,
     private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.profileForm =  this.formBuilder.group({
+      title: ['', [Validators.required, Validators.minLength(4)]],
+      body: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  
     const id = this.activatedRoute.snapshot.paramMap.get('id')
     this.httpClient.get('https://jsonplaceholder.typicode.com/posts/' + id)
     .pipe(map((res:any) => {
@@ -34,27 +35,32 @@ export class EditPostComponent implements OnInit {
       console.log(res);
       this.item = res;
     });
-
-    // void {
-    //   this.profileForm = new FormGroup({
-    //     'title': new FormControl(this.)
-    //   })
-    // }
   }
 
-  onSubmit() {
+  onSubmit(post: Post) {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    post = this.profileForm.value;
+    this.httpClient.put('https://jsonplaceholder.typicode.com/posts/' + id, post)
+    .subscribe((res: Post) => {
+      this.item = res;
+      console.log(res)
+    })
     console.warn(this.profileForm.value);
     console.warn(this.profileForm.value.title);
     console.warn(this.profileForm.value.body);
-  }
-
-  editPost() {
-    const returnedTarget = Object.assign(this.item, this.profileForm.value);
-    console.log(returnedTarget);
-    return returnedTarget;
-  }
-
-  redirectToPostsPage() {
+    if(this.profileForm.invalid) {
+      return;
+    }
     this.router.navigateByUrl('/posts');
   }
+
+  // editPost() {
+  //   const returnedTarget = Object.assign(this.item, this.profileForm.value);
+  //   console.log(returnedTarget);
+  //   return returnedTarget;
+  // }
+
+  // redirectToPostsPage() {
+  //   this.router.navigateByUrl('/posts');
+  // }
 }
